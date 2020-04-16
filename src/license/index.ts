ff8469fc6ad1168533ddcb8a084080ae16f68b2a
Leonty Chudinov <leonty.chudinov@gmail.com>
2020-04-16 09:22:52 +0500

    Support yaml files

--- src/license/index.ts
+++ src/license/index.ts
@@ -2,7 +2,7 @@ import { Rule, SchematicContext, Tree, chain } from '@angular-devkit/schematics'
 
 const eol = '\r\n';
 
-const licenseText = [
+const licenseLines = [
   `  This program and the accompanying materials are`,
   `  made available under the terms of the Eclipse Public License v2.0 which accompanies`,
   `  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html`,
@@ -10,9 +10,9 @@ const licenseText = [
   `  SPDX-License-Identifier: EPL-2.0`,
   ``,
   `  Copyright Contributors to the Zowe Project.`,
-].join(eol);
+];
 
-const license2Text = [
+const license2Lines = [
   `  This program and the accompanying materials are`,
   `  made available under the terms of the Eclipse Public License v2.0 which accompanies`,
   `  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html`,
@@ -20,7 +20,9 @@ const license2Text = [
   `  SPDX-License-Identifier: EPL-2.0`,
   `  `,
   `  Copyright Contributors to the Zowe Project.`,
-].join(eol);
+];
+
+
 export function license(_options: any): Rule {
   return chain([
     (tree: Tree, _context: SchematicContext) => {
@@ -30,27 +32,40 @@ export function license(_options: any): Rule {
       }
       tree.getDir(_options.sourceDir)
         .visit(filePath => {
-          if (!filePath.endsWith('.ts') && !filePath.endsWith('.css') && !filePath.endsWith('.scss') && !filePath.endsWith('.html')) {
+          if (!filePath.endsWith('.ts') && !filePath.endsWith('.css') && !filePath.endsWith('.scss') && !filePath.endsWith('.html') && !filePath.endsWith('.yaml')) {
             return;
           }
           const content = tree.read(filePath);
           if (!content) {
             return;
-          } else {
           }
-          let beginComment = '/*'
-          let endComment = '*/'
-          // Prevent from writing license to files that already have one.
-          if (content.indexOf(licenseText) == -1 && content.indexOf(license2Text) == -1) {
-            if (filePath.endsWith('.html')) {
-              beginComment = '<!--';
-              endComment = '-->';
+
+          if (filePath.endsWith('.yaml')) {
+            const licenseText = licenseLines.map(line => `#${line}`).join(eol);
+            const license2Text = license2Lines.map(line => `#${line}`).join(eol);
+            if (content.indexOf(licenseText) == -1 && content.indexOf(license2Text) == -1) {
+              const license = licenseText + eol;
+              tree.overwrite(
+                filePath,
+                license + eol + content + eol + eol + license
+              );
             }
-            const license = beginComment + eol + licenseText + eol + endComment + eol;
-            tree.overwrite(
-              filePath,
-              license + eol + content + eol + eol + license
-            );
+          } else {
+            const licenseText = licenseLines.join(eol);
+            const license2Text = license2Lines.join(eol);
+            if (content.indexOf(licenseText) == -1 && content.indexOf(license2Text) == -1) {
+              let beginComment = '/*'
+              let endComment = '*/'
+              if (filePath.endsWith('.html')) {
+                beginComment = '<!--';
+                endComment = '-->';
+              }
+              const license = beginComment + eol + licenseText + eol + endComment + eol;
+              tree.overwrite(
+                filePath,
+                license + eol + content + eol + eol + license
+              );
+            }
           }
         });
       return tree;
